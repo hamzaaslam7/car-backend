80af0f5f76acd2bbdfe26b48dd99837775c84d21
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:30:25 +0500

    uncomments code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const keys = require("./config/key");
 
 mongoose.Promise = global.Promise;
 mongoose.set('strictQuery', false);
-mongoose.connect(keys.mongoURI).then((con) => {
+mongoose.connect("mongodb+srv://hamza:p9kmTfuAjUngXVem@cluster0.rbauogk.mongodb.net/carServiceDb?retryWrites=true&w=majority&appName=Cluster0").then((con) => {
   console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
 });
 
@@ -16,10 +16,10 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// process.on('unhandledRejection', err => {
-//     console.log(err.name, err.message);
-//     console.log('UnHandel rejection');
-//     server.close(() => {
-//         process.exit(1);
-//     });
-// });
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UnHandel rejection');
+    server.close(() => {
+        process.exit(1);
+    });
+});
