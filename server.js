2941104524fb7280343b5601b3e4b63843dcb029
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:47:37 +0500

    uncomments code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 //const keys = require("./config/key");
 
 
@@ -15,10 +15,10 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// process.on('unhandledRejection', err => {
-//     console.log(err.name, err.message);
-//     console.log('UnHandel rejection');
-//     server.close(() => {
-//         process.exit(1);
-//     });
-// });
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UnHandel rejection');
+    server.close(() => {
+        process.exit(1);
+    });
+});
