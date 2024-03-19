8cfeec19d2279429706e4907d3d4c02fadf8f19f
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:05:51 +0500

    vercel file change

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,5 @@
 const AppError = require("../utils/appError");
+const keys = require("../config/key");
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -51,9 +52,9 @@ module.exports = (err, req, res, next) => {
   console.log(err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  if (process.env.NODE_ENV === "development") {
+  if (keys.NODE_ENV === "development") {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === "production") {
+  } else if (keys.NODE_ENV === "production") {
     let error = { ...err };
     if (error.kind === "ObjectId") error = handleCastErrorDb(error);
     if (error.code === 11000) error = handleDuplicateField(error);
