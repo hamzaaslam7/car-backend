3be9a0eb321b4e87f96b1c3bc104d84d7fc28045
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:52:08 +0500

    uncomments code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 //const keys = require("./config/key");
 
 
