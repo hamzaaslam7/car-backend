a97b722b1276f5f5433972dbcfbe3c24b0d8320d
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:20:16 +0500

    code comments

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
-const mongoose = require("mongoose");
-const keys = require("./config/key");
+// const mongoose = require("mongoose");
+// const keys = require("./config/key");
 
-mongoose.Promise = global.Promise;
-mongoose.set('strictQuery', false);
-mongoose.connect(keys.mongoURI).then((con) => {
-  console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
-});
+// mongoose.Promise = global.Promise;
+// mongoose.set('strictQuery', false);
+// mongoose.connect(keys.mongoURI).then((con) => {
+//   console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
+// });
 
 
 const port = process.env.PORT || 4000;
@@ -16,10 +16,10 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-process.on('unhandledRejection', err => {
-    console.log(err.name, err.message);
-    console.log('UnHandel rejection');
-    server.close(() => {
-        process.exit(1);
-    });
-});
+// process.on('unhandledRejection', err => {
+//     console.log(err.name, err.message);
+//     console.log('UnHandel rejection');
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
