775bd2daefda2e0546048f695f70bb801dea73e4
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 09:22:10 +0500

    add env variable

--- config/pro.js
+++ config/pro.js
@@ -2,5 +2,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   JWT_SECRET: process.env.JWT_SECRET,
+  JWT_COOKIE_EXPIRES_IN:process.env.JWT_COOKIE_EXPIRES_IN,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 };
