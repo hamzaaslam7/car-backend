40ae0215e5778ef13d3e3f9c3876dd52016ddc8f
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:53:04 +0500

    uncomments code

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 const mongoose = require("mongoose");
-//const keys = require("./config/key");
+const keys = require("./config/key");
 
-
-// mongoose.connect("mongodb+srv://hamza:p9kmTfuAjUngXVem@cluster0.rbauogk.mongodb.net/carServiceDb?retryWrites=true&w=majority&appName=Cluster0").then((con) => {
-//   console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
-// });
+mongoose.Promise = global.Promise;
+mongoose.set('strictQuery', false);
+mongoose.connect(keys.mongoURI).then((con) => {
+  console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
+});
 
 
 const port = process.env.PORT || 4000;
