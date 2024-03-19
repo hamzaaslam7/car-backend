5ee894d34af878d443dcfe9f19f8bf8917b00f1f
hamza-at-rs <nestjs.rsdev@reactivespace.com>
2024-03-19 10:39:23 +0500

    uncomments code

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const mongoose = require("mongoose");
 //const keys = require("./config/key");
 
 
-mongoose.connect("mongodb+srv://hamza:p9kmTfuAjUngXVem@cluster0.rbauogk.mongodb.net/carServiceDb?retryWrites=true&w=majority&appName=Cluster0").then((con) => {
-  console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
-});
+// mongoose.connect("mongodb+srv://hamza:p9kmTfuAjUngXVem@cluster0.rbauogk.mongodb.net/carServiceDb?retryWrites=true&w=majority&appName=Cluster0").then((con) => {
+//   console.log(":::::::::::::: Connection Success !!! :::::::::::::::::::");
+// });
 
 
 const port = process.env.PORT || 4000;
@@ -15,10 +15,10 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// process.on('unhandledRejection', err => {
-//     console.log(err.name, err.message);
-//     console.log('UnHandel rejection');
-//     server.close(() => {
-//         process.exit(1);
-//     });
-// });
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UnHandel rejection');
+    server.close(() => {
+        process.exit(1);
+    });
+});
